bd2f00ababe7ecabc86825f6bc4f176c62ab0a81
Gustavo-Mendess <gustavormendess@gmail.com>
2024-02-20 09:24:02 -0400

    Estrutura NextJS

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,2 +1,19 @@
-import {} from 'n'
-import Head from ''
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Cart = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Carrinho
+      </h1>
+    </>
+  );
+};
+
+export default Cart;
